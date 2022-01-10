16fdfd13d642039f02ff5242927e4db77e1f8ab7
Cloudbop <boppincolin@gmail.com>
2022-01-10 23:08:17 +0000

    +query-demo +useGQLRQ

---
+++ src/pages/query-demo.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Layout from "../components/Layout";
+import Demo from "../components/rq-demo";
+
+export default function SetPassword() {
+  return (
+    <Layout>
+      <h1>RQ-demo</h1>
+      <Demo />
+    </Layout>
+  );
+}
