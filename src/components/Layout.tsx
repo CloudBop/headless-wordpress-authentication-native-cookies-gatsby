22453d1f5191a40bcedb0ef11230b454f36ea474
Cloudbop <boppincolin@gmail.com>
2022-01-10 23:07:40 +0000

    +rq +useAuth provider

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,23 @@
 import * as React from "react";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { AuthProvider } from "../hooks/useAuth";
 
 import Header from "./Header";
 
+const queryClient = new QueryClient();
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <title>Headless WP App</title>
-      <Header />
-      <main>{children}</main>
-    </>
+    <QueryClientProvider client={queryClient}>
+      {/* auth provider requires QueryClient for requests */}
+      <AuthProvider>
+        <title>Headless WP App</title>
+        <Header />
+        <main>{children}</main>
+      </AuthProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
