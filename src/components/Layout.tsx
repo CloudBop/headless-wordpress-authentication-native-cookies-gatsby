d4af57b0492d6fa9d30e35391aa32cb91acecf7b
Cloudbop <boppincolin@gmail.com>
2022-01-16 13:23:15 +0000

    -useAuthBasicContext

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { AuthProvider } from "../hooks/useAuth";
+
+// import { AuthProvider } from "../hooks/deprectated/useAuthBasicContext";
 
 import Header from "./Header";
 
@@ -11,12 +12,14 @@ const queryClient = new QueryClient();
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <title>Headless WP App</title>
+      <Header />
+      <main>{children}</main>
+
       {/* auth provider requires QueryClient for requests */}
-      <AuthProvider>
-        <title>Headless WP App</title>
-        <Header />
-        <main>{children}</main>
-      </AuthProvider>
+      {/* <AuthProvider> */}
+      {/* </AuthProvider> */}
+
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
