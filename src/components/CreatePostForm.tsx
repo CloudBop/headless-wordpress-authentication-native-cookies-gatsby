dd41d14e2c84aba3d63d3d4ba7831835448bb6d6
Cloudbop <boppincolin@gmail.com>
2022-01-16 21:50:44 +0000

    ~updated CustomPost CustomPostForm

--- src/components/CreatePostForm.tsx
+++ src/components/CreatePostForm.tsx
@@ -1,64 +1,67 @@
 import * as React from "react";
-import { useMutation, gql } from "@apollo/client";
+// import { useMutation, gql } from "@apollo/client";
+import AuthContent from "./AuthContent";
+import { useUser } from "../hooks/useUser";
+import { useMutation } from "react-query";
+import { wpgraphqlCreateNewPost } from "../wpgraphqlApi/userSettings";
 
-const CREATE_POST = gql`
-  mutation createPost($title: String!, $content: String!) {
-    createPost(input: {
-      title: $title
-      content: $content
-      status: PUBLISH
-    }) {
-      post {
-        databaseId
-      }
-    }
-  }
-`;
+// const CREATE_POST = gql`
+//   mutation createPost($title: String!, $content: String!) {
+//     createPost(input: { title: $title, content: $content, status: PUBLISH }) {
+//       post {
+//         databaseId
+//       }
+//     }
+//   }
+// `;
 
 export default function CreatePostForm() {
-  const [createPost, { data, loading, error }] = useMutation(CREATE_POST);
+  const { user } = useUser();
+  const canCreatePosts = Boolean(
+    user?.viewer?.capabilities?.includes("publish_posts")
+  );
+  // const [createPost, { data, loading, error }] = useMutation(CREATE_POST);
+
+  const { data, isLoading, error } = useMutation(wpgraphqlCreateNewPost);
+
   const wasPostCreated = Boolean(data?.createPost?.post?.databaseId);
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const values = Object.fromEntries(data);
-    createPost({
-      variables: values
-    }).catch(error => {
-      console.error(error);
-    });
+    // createPost({
+    //   variables: values,
+    // }).catch((error) => {
+    //   console.error(error);
+    // });
   }
 
   if (wasPostCreated) {
-    return (
-      <p>Post successfully created.</p>
-    );
+    return <p>Post successfully created.</p>;
   }
 
   return (
-    <form method="post" onSubmit={handleSubmit}>
-      <fieldset disabled={loading} aria-busy={loading}>
-        <label htmlFor="create-post-title">Title</label>
-        <input
-          id="create-post-title"
-          type="text"
-          name="title"
-          required
-        />
-        <label htmlFor="creat-post-content">Content</label>
-        <textarea
-          id="creat-post-content"
-          name="content"
-          required
-        />
-        {error ? (
-          <p className="error-message">{error.message}</p>
-        ) : null}
-        <button type="submit" disabled={loading}>
-          {loading ? 'Creating post...' : 'Create post'}
-        </button>
-      </fieldset>
-    </form>
-  )
+    <AuthContent>
+      <h1>Create Post</h1>
+      {canCreatePosts ? (
+        <form method="post" onSubmit={handleSubmit}>
+          <fieldset disabled={isLoading} aria-busy={isLoading}>
+            <label htmlFor="create-post-title">Title</label>
+            <input id="create-post-title" type="text" name="title" required />
+            <label htmlFor="creat-post-content">Content</label>
+            <textarea id="creat-post-content" name="content" required />
+            {error instanceof Error ? (
+              <p className="error-message">{error.message}</p>
+            ) : null}
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Creating post..." : "Create post"}
+            </button>
+          </fieldset>
+        </form>
+      ) : (
+        <p>You don&#39;t have the permissions necessary to create posts.</p>
+      )}
+    </AuthContent>
+  );
 }
--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,26 +1,12 @@
 import * as React from "react";
 
-import AuthContent from "../components/AuthContent";
 import Layout from "../components/Layout";
 import CreatePostForm from "../components/CreatePostForm";
-import { useUser } from "../hooks/useUser";
 
 export default function CreatePost() {
-  const { user } = useUser();
-  const canCreatePosts = Boolean(
-    user?.viewer?.capabilities?.includes("publish_posts")
-  );
-
   return (
     <Layout>
-      <AuthContent>
-        <h1>Create Post</h1>
-        {canCreatePosts ? (
-          <CreatePostForm />
-        ) : (
-          <p>You don&#39;t have the permissions necessary to create posts.</p>
-        )}
-      </AuthContent>
+      <CreatePostForm />
     </Layout>
   );
 }
