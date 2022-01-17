a63bdaa353726d3934966106ff6d020f8e57bcc4
Cloudbop <boppincolin@gmail.com>
2022-01-17 22:19:38 +0000

    uninstalled @apollo/client

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,13 @@
 import * as React from "react";
-import { ApolloProvider } from "@apollo/client";
+// import { ApolloProvider } from "@apollo/client";
 
-import { client } from "./lib/apolloClient";
+// import { client } from "./lib/apolloClient";
 // import { AuthProvider } from "./src/hooks/useAuth";
 import "./src/styles/global.css";
 
 export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>
+  <>
     {/* <AuthProvider>{}</AuthProvider> */}
     {element}
-  </ApolloProvider>
+  </>
 );
